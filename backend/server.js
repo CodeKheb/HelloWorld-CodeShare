422fbcb14c4ca411a0cfd3440489332ed5780d78
CodeKheb <kherbinbuenaventura@gmail.com>
2026-05-04 02:41:54 +0800

    Added: OAuth2

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, "../.env") });
+dotenv.config({ path: path.join(__dirname, ".env") });
+
+import "./server.js";
 
 import { Server } from "socket.io";
 import { app } from "./express/express.js";
