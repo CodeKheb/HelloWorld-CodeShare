d0bc8c688241e1e5b3693a085e4ff733449f43b1
J0sH-code <joshrylesanteno07@gmail.com>
2026-05-03 23:49:50 +0800

    Routing done in express

--- backend/express/express.js
+++ backend/express/express.js
@@ -13,15 +13,18 @@ const __dirname = path.dirname(__filename)
 console.log(port);
 
 app.use(express.urlencoded({ extended: true })); //Used to read and understand data sent from HTML forms
-app.use(express.static(path.join(__dirname, "../../frontend"))) //Serve static frontend files
+app.use(express.static(path.join(__dirname, "../../frontend"), { index: false })) //Serve static frontend files
 app.use(express.json())
 
 app.use("/api/auth", authRouter)
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend", 'index.html'));
+  res.sendFile(path.join(__dirname, "../../frontend", 'login.html'));
 })
 
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend", 'index.html'));
+})
 
 app.get("/hello", (req, res) => {
     res.send({message: "hello"})
