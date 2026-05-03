2b585528022a100f4cc9ec93740fe964444fc9d7
J0sH-code <joshrylesanteno07@gmail.com>
2026-05-03 22:12:17 +0800

    Started socket.io integration

---
+++ backend/express/express.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+console.log(port);
+
+app.use(express.urlencoded({ extended: true })); //Used to read and understand data sent from HTML forms
+app.use(express.static(path.join(__dirname, "../../frontend"))) //Serve static frontend files
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend", 'index.html'));
+})
+
+app.get("/hello", (req, res) => {
+    res.send({message: "hello"})
+})
+
+export default app;
