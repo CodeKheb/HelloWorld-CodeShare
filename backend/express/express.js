f45dbdbe4e46e60fb5c6d3b3f9b32248496790c6
CodeKheb <kherbinbuenaventura@gmail.com>
2026-05-05 03:14:45 +0800

    deploy

--- backend/express/express.js
+++ backend/express/express.js
@@ -35,7 +35,7 @@ export const sessionMiddleware = session({
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: false,
+        secure: true,
         maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
 });
