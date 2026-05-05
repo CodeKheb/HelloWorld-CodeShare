4755b13c50dd69652ac848ae90079c31b04408fd
CodeKheb <kherbinbuenaventura@gmail.com>
2026-05-06 00:01:07 +0800

    Deploy

--- frontend/js/messages.js
+++ frontend/js/messages.js
@@ -1,6 +1,6 @@
 import { io } from "https://cdn.socket.io/4.5.4/socket.io.esm.min.js";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://codeshare-ewmi.onrender.com", {
     withCredentials: true
 });
 
